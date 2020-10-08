80c9b9632691aac474c766a33fcb3af843b1b3db
MarioTorres09032018 <mario2torres@gmail.com>
2020-10-08 03:15:10 -0300

    editando novas classes

--- Pessoa/Pessoa.cs
+++ Pessoa/Pessoa.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace Aula
@@ -40,17 +41,19 @@ namespace Aula
             {
                 Directory.CreateDirectory(PASTA);
             }
-            if(!File.Exists(PATHFILE))
-            {
-                File.Create(PATHFILE);
-            }
+
+            Console.WriteLine(File.Exists(PATHFILE));
+            File.WriteAllText(PATHFILE, "CadastroUsuario");
+            Console.WriteLine(File.Exists(PATHFILE));
+            Console.ReadKey(true);
 
             //salva em um arquivo de txt
             File.WriteAllText(PATHFILE, JsonConvert.SerializeObject(this) + "\n" +  dadoscarregados);
             
         }
-
+        
         public static string CarregarDadosArquivo()
+        
         {
             return File.ReadAllText(PATHFILE);
         }
